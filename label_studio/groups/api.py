5e7583fcc725fd5a6974bd04920655630211cec7
ErfannNikooee <nikooeeerfan@gmail.com>
2024-01-10 00:22:28 +0330

    Added some of apies

--- label_studio/groups/api.py
+++ label_studio/groups/api.py
@@ -10,7 +10,10 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from groups.models import Group, GroupMember
 from groups.serializers import (
-    GroupIdSerializer
+    GroupIdSerializer,
+    GroupMemberUserSerializer,
+    GroupSerializer,
+    GroupsParamsSerializer,
 )
 from rest_framework import generics, status
 from rest_framework.exceptions import NotFound, PermissionDenied
@@ -29,17 +32,17 @@ class GroupListAPI(generics.ListCreateAPIView):
     queryset = Group.objects.all()
     parser_classes = (JSONParser, FormParser, MultiPartParser)
     permission_required = ViewClassPermission(
-        GET=all_permissions.organizations_view,
-        PUT=all_permissions.organizations_change,
-        POST=all_permissions.organizations_create,
-        PATCH=all_permissions.organizations_change,
-        DELETE=all_permissions.organizations_change,
+        GET=all_permissions.groups_view,
+        PUT=all_permissions.groups_change,
+        POST=all_permissions.groups_create,
+        PATCH=all_permissions.groups_change,
+        DELETE=all_permissions.groups_change,
     )
     serializer_class = GroupIdSerializer
 
     def filter_queryset(self, queryset):
         return queryset.filter(
-            organizationmember__in=self.request.user.om_through.filter(deleted_at__isnull=True)
+            groupmember__in=self.request.user.om_through.filter(deleted_at__isnull=True)
         ).distinct()
 
     def get(self, request, *args, **kwargs):
@@ -48,3 +51,66 @@ class GroupListAPI(generics.ListCreateAPIView):
     @swagger_auto_schema(auto_schema=None)
     def post(self, request, *args, **kwargs):
         return super(GroupListAPI, self).post(request, *args, **kwargs)
+
+class GroupMemberPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+
+    def get_page_size(self, request):
+        # emulate "unlimited" page_size
+        if (
+            self.page_size_query_param in request.query_params
+            and request.query_params[self.page_size_query_param] == '-1'
+        ):
+            return 1000000
+        return super().get_page_size(request)
+    
+@method_decorator(
+    name='get',
+    decorator=swagger_auto_schema(
+        tags=['Groups'],
+        operation_summary='Get group members list',
+        operation_description='Retrieve a list of the group members and their IDs.',
+        manual_parameters=[
+            openapi.Parameter(
+                name='id',
+                type=openapi.TYPE_INTEGER,
+                in_=openapi.IN_PATH,
+                description='A unique integer value identifying this group.',
+            ),
+        ],
+    ),
+)
+class GroupMemberListAPI(generics.ListAPIView):
+    parser_classes = (JSONParser, FormParser, MultiPartParser)
+    permission_required = ViewClassPermission(
+        GET=all_permissions.groups_view,
+        PUT=all_permissions.groups_change,
+        PATCH=all_permissions.groups_change,
+        DELETE=all_permissions.groups_change,
+    )
+    serializer_class = GroupMemberUserSerializer
+    pagination_class = GroupMemberPagination
+
+    def get_serializer_context(self):
+        return {
+            'contributed_to_projects': bool_from_request(self.request.GET, 'contributed_to_projects', False),
+            'request': self.request,
+        }
+
+    def get_queryset(self):
+        org = generics.get_object_or_404(self.request.user.organizations, pk=self.kwargs[self.lookup_field])
+        if flag_set('fix_backend_dev_3134_exclude_deactivated_users', self.request.user):
+            serializer = GroupsParamsSerializer(data=self.request.GET)
+            serializer.is_valid(raise_exception=True)
+            active = serializer.validated_data.get('active')
+
+            # return only active users (exclude DISABLED and NOT_ACTIVATED)
+            if active:
+                return org.active_members.order_by('user__username')
+
+            # organization page to show all members
+            return org.members.order_by('user__username')
+        else:
+            return org.members.order_by('user__username')
+
