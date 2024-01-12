3fb12d867ba7cd2b6acb27b07320ee234b946555
ErfannNikooee <nikooeeerfan@gmail.com>
2024-01-12 23:23:30 +0330

    added group urls

--- label_studio/groups/urls.py
+++ label_studio/groups/urls.py
@@ -1,8 +1,29 @@
 from django.urls import include, path
-from groups import  views
+from groups import  api, views
 
 app_name = 'groups'
 
+_urlpatterns = [
+    path('',views.group_list, name='group-index')
+]
+
+_api_urlpattens = [
+    path('', api.GroupListAPI.as_view(), name='group-list'),
+    path('<int:pk>', api.GroupAPI.as_view(), name='group-detail'),
+    path('<int:pk>/memberships', api.GroupMemberListAPI.as_view(), name='group-memberships-list'),
+    path(
+        '<int:pk>/memberships/<int:user_pk>/',
+        api.GroupMemberDetailAPI.as_view(),
+        name='group-membership-detail',
+    ),
+]
+
 urlpatterns = [
-    path('group/',views.index, name='group-view')
+    path('group/', views.simple_view, name='group-simple'),
+    path('group/webhooks', views.simple_view, name='group-simple-webhooks'),
+    path('members/', include(_urlpatterns)),
+    path('api/groups/', include((_api_urlpattens, app_name), namespace='api')),
+    # # invite
+    # path('api/invite', api.OrganizationInviteAPI.as_view(), name='organization-invite'),
+    # path('api/invite/reset-token', api.OrganizationResetTokenAPI.as_view(), name='organization-reset-token'),
 ]
