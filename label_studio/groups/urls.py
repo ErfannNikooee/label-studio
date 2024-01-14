525094cb6d87f7fddc3f51ef4b638944537f87c2
ErfannNikooee <nikooeeerfan@gmail.com>
2024-01-14 16:56:57 +0330

    fixed api

--- label_studio/groups/urls.py
+++ label_studio/groups/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('group/', views.simple_view, name='group-simple'),
     path('group/webhooks', views.simple_view, name='group-simple-webhooks'),
     path('members/', include(_urlpatterns)),
-    path('api/groups/', include((_api_urlpattens, app_name), namespace='api')),
+    path('api/group/', include((_api_urlpattens, app_name), namespace='api')),
     # # invite
     # path('api/invite', api.OrganizationInviteAPI.as_view(), name='organization-invite'),
     # path('api/invite/reset-token', api.OrganizationResetTokenAPI.as_view(), name='organization-reset-token'),
