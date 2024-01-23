b87301477fd5274cfcd6f4fb5513dba4058b3e15
ErfannNikooee <nikooeeerfan@gmail.com>
2024-01-24 00:03:43 +0330

    registered group and group memeber models

--- label_studio/groups/admin.py
+++ label_studio/groups/admin.py
@@ -1,3 +1,7 @@
 # from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from groups.models import GroupMember, Group
+admin.site.register(Group)
+admin.site.register(GroupMember)
