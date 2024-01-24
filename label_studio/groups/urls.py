00c7985b8fbddc90a5d920781eb79c8a3d8a4219
ErfannNikooee <nikooeeerfan@gmail.com>
2024-01-25 01:10:06 +0330

    fixed routes and views

--- label_studio/groups/urls.py
+++ label_studio/groups/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from groups import  api, views
+from groups import api, views
 
 app_name = 'groups'
 
@@ -10,9 +10,9 @@ _urlpatterns = [
 _api_urlpattens = [
     path('', api.GroupListAPI.as_view(), name='group-list'),
     path('<int:pk>', api.GroupAPI.as_view(), name='group-detail'),
-    path('<int:pk>/memberships', api.GroupMemberListAPI.as_view(), name='group-memberships-list'),
+    path('<int:pk>/members', api.GroupMemberListAPI.as_view(), name='group-memberships-list'),
     path(
-        '<int:pk>/memberships/<int:user_pk>/',
+        '<int:group_id>/members/<int:user_pk>',
         api.GroupMemberDetailAPI.as_view(),
         name='group-membership-detail',
     ),
@@ -23,7 +23,7 @@ urlpatterns = [
     path('group/webhooks', views.simple_view, name='group-simple-webhooks'),
     path('members/', include(_urlpatterns)),
     path('api/group/', include((_api_urlpattens, app_name), namespace='api')),
-    path('api/newgroup/',api.GroupAPI,name='creategroup' )
+    path('api/newgroup/',api.GroupAPI.as_view(),name='creategroup' )
     # # invite
     # path('api/invite', api.OrganizationInviteAPI.as_view(), name='organization-invite'),
     # path('api/invite/reset-token', api.OrganizationResetTokenAPI.as_view(), name='organization-reset-token'),
