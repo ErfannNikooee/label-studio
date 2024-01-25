af439f74436e49be215532d9e70fe62d19393bd6
ErfannNikooee <nikooeeerfan@gmail.com>
2024-01-25 16:10:05 +0330

    fixed a bug and updated front end

--- label_studio/frontend/src/assets/icons/index.js
+++ label_studio/frontend/src/assets/icons/index.js
@@ -1,7 +1,4 @@
 export { ReactComponent as IconAllProjects } from './all-projects.svg';
-export {ReactComponent as SBUlogo} from './sbu-logo.svg';
-// export {ReactComponent as NLPLABLogo} from '';
-// export {ReactComponent as NLPLABTitle} from'';
 export { ReactComponent as IconBell } from './bell.svg';
 export { ReactComponent as IconBook } from './book.svg';
 export { ReactComponent as LsBulb } from './bulb.svg';
